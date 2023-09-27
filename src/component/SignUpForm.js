23ec5f2066834d4cc68386c327405e470baf6ca1
Kellin Titenko <xkellinx@gmail.com>
2023-09-27 13:05:03 +0300

    signup form add not working ete

--- src/component/SignUpForm.js
+++ src/component/SignUpForm.js
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUpForm = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [formMessage, setFormMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        // Update the corresponding state variable based on the input name
+        switch (name) {
+            case 'firstName':
+                setFirstName(value);
+                break;
+            case 'lastName':
+                setLastName(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'username':
+                setUsername(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            case 'confirmPassword':
+                setConfirmPassword(value);
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (
+            firstName &&
+            lastName &&
+            email &&
+            username &&
+            password &&
+            confirmPassword &&
+            password.length >= 6 &&
+            password === confirmPassword
+        ) {
+            console.log('Form data:', {
+                firstName,
+                lastName,
+                email,
+                username,
+                password,
+                confirmPassword,
+            });
+            setFormMessage(''); // Clear the form message if the form is valid
+        } else {
+            setFormMessage('Please fill in all the fields before signing up.');
+        }
+    };
+
     return (
         <div className="contact-form">
             <h1>Sign Up</h1>
             <section>
-                <form action="#" method="POST">
+                <form onSubmit={handleSubmit}>
                     <fieldset>
                         <legend>
                             <strong>Sign Up Form</strong>
@@ -20,6 +82,8 @@ const SignUpForm = () => {
                             name="firstName"
                             className="form-input"
                             placeholder="First Name"
+                            value={firstName}
+                            onChange={handleInputChange}
                         />
 
                         <label htmlFor="lastName">
@@ -31,6 +95,8 @@ const SignUpForm = () => {
                             name="lastName"
                             className="form-input"
                             placeholder="Last Name"
+                            value={lastName}
+                            onChange={handleInputChange}
                         />
 
                         <label htmlFor="email">
@@ -42,6 +108,8 @@ const SignUpForm = () => {
                             name="email"
                             className="form-input"
                             placeholder="Email"
+                            value={email}
+                            onChange={handleInputChange}
                         />
 
                         <label htmlFor="username">
@@ -53,6 +121,8 @@ const SignUpForm = () => {
                             name="username"
                             className="form-input"
                             placeholder="Username"
+                            value={username}
+                            onChange={handleInputChange}
                         />
 
                         <label htmlFor="password">
@@ -64,7 +134,8 @@ const SignUpForm = () => {
                             name="password"
                             className="form-input"
                             placeholder="Password (at least 6 characters)"
-                            minLength="6"
+                            value={password}
+                            onChange={handleInputChange}
                         />
 
                         <label htmlFor="confirmPassword">
@@ -76,17 +147,14 @@ const SignUpForm = () => {
                             name="confirmPassword"
                             className="form-input"
                             placeholder="Confirm Password"
-                            minLength="6"
+                            value={confirmPassword}
+                            onChange={handleInputChange}
                         />
 
-                        <button
-                            disabled
-                            className="subButton"
-                            type="submit"
-                            value="Sign Up"
-                        >
+                        <button className="subButton" type="submit" value="Sign Up">
                             <strong>Sign Up</strong>
                         </button>
+                        <div className="form-message">{formMessage}</div>
                     </fieldset>
                 </form>
             </section>
