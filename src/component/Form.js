0e409f9a21666b4f80a733b171f1c3acb13cd86a
Kellin Titenko <xkellinx@gmail.com>
2023-09-27 10:25:24 +0300

    starting to work on the form

---
+++ src/component/Form.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+const ContactForm = () => {
+  return (
+    <div className="contact-form">
+      <h1>Contact Form</h1>
+      <section>
+        <form action="https://formspree.io/f/mzbqqowg" method="POST">
+          <fieldset>
+            <legend>
+              <strong>Fill out the form below to contact me</strong>
+            </legend>
+
+            <label htmlFor="firstName">
+              <strong>Your first name</strong>
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              className="form-input"
+            />
+
+            <label htmlFor="lastName">
+              <strong>Your last name</strong>
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              placeholder="(optional)"
+              className="form-input"
+            />
+
+            <label htmlFor="email">
+              <strong>Email address</strong>
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="form-input"
+            />
+
+            <label htmlFor="reason">
+              <strong>Reason for contacting me:</strong>
+            </label>
+            <select id="reason" name="reason" className="form-input">
+              <option selected>Select one...</option>
+              <option value="job">Job offer</option>
+              <option value="collab">Collaboration on a project</option>
+              <option value="fly">I want to fly with you to see cool cities</option>
+              <option value="other">Other</option>
+            </select>
+
+            <label htmlFor="comments">
+              <strong>Tell me more about it:</strong>
+            </label>
+            <textarea
+              id="comments"
+              maxLength="250"
+              name="comments"
+              className="form-textarea"
+            ></textarea>
+            <p></p>
+
+            <input
+              id="send_to"
+              type="hidden"
+              name="send_to"
+              value="xkellinx@gmail.com"
+            />
+            <button
+              disabled
+              className="subButton"
+              type="submit"
+              value="Send"
+            >
+              <strong>Submit</strong>
+            </button>
+          </fieldset>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ src/component/OpeningPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const OpeningPage = () => {
+  return (
+    <div>
+    <div>title</div>
+    <div>animation</div>
+    <div>making your trip safer sines 28/09/23 14:00 pm</div>
+    </div>
+    
+
+  )
+}
+
+export default OpeningPage
