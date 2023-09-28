311693935999fb094df7b96d421ca42d1ad7db47
evgenyrinkevich <rinkevich2000@gmail.com>
2023-09-28 11:48:09 +0300

    login/logout

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ const App = () => {
     <OpeningPage />
   ) : (
     <div className="App">
-      <NavBar />
+      <NavBar user={user} />
       {isLoaded ? (
         <>
           <Routes>
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import OpeningPage from './OpeningPage';
+import React from "react";
+import { Link } from "react-router-dom";
+import OpeningPage from "./OpeningPage";
 
-const NavBar = () => {
-    return (
-        <div className="navbar">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/report-scam" className="nav-link">Report Scam</Link>
-            <Link to="/login" className="nav-link">Login</Link>
-        </div>
-    );
-}
+const NavBar = ({ user }) => {
+  return (
+    <div className="navbar">
+      <Link to="/" className="nav-link">
+        Home
+      </Link>
+      <Link to="/report-scam" className="nav-link">
+        Report Scam
+      </Link>
+      {user ? (
+        <Link to="/login" className="nav-link">
+          Logout
+        </Link>
+      ) : (
+        <Link to="/login" className="nav-link">
+          Login
+        </Link>
+      )}
+    </div>
+  );
+};
 
 export default NavBar;
